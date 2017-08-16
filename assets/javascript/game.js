f475ae6c8697bc079eb16d56fa1cb3ec8f717e0b
yaw owusu <asirifiyaw@gmail.com>
2017-08-16 07:00:53 -0700

    more progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,33 +6,38 @@ console.log(randomLetter);
 
 var wins = 0;
 var losses = 0;
-var guessesLeft = 3;
-var guessesSoFar = 0;
-var guessedLetter = [];
+var guessesLeft = 5;
+var guessedLetters = [];
+
+var reset = function(){
+		guessesLeft=5;
+		guessedLetters=[];
+	};
 
 
 
 document.getElementById("wins").innerHTML = "Wins: " + wins;
 document.getElementById("losses").innerHTML = "Losses: " + losses;
 document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
-document.getElementById("guessesSoFar").innerHTML = "Your Guesses So Far: " + guessesSoFar;
+document.getElementById("guessesSoFar").innerHTML = "Guesses So Far: " + guessedLetters;
 
-	function reset(){
-	wins=0;
-	losses=0;
-	guessesLeft=0;
-	guessesSoFar=0;
 
-	};
+
 
 
 
 	onkeyup=function(event){
 	var playerGuess = event.key.toLowerCase();
 	guessesLeft--;
-	document.getElementById("guessesLeft").innerHTML = "Gusses Left: " + guessesLeft;
-	/*This will dynamically update HTML"*/
+	document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+	/*This will dynamically update guesses left on the HTML page"*/
 
+
+	guessedLetters.push(playerGuess);
+	console.log(guessedLetters);
+	/*push will update the guessedLetters array - place new entry at the end*/
+	document.getElementById("guessesSoFar").innerHTML = "Guesses So Far: " + guessedLetters;
+	/*This will dynamically update guesses so far on the HTML page"*/
 
 	if (guessesLeft > 0 && playerGuess == randomLetter){
 		wins++;
@@ -40,11 +45,12 @@ document.getElementById("guessesSoFar").innerHTML = "Your Guesses So Far: " + gu
 		reset();
 	}
 	if (guessesLeft == 0){
+		reset();
 		losses++;
 		document.getElementById("losses").innerHTML = "Losses: " + losses;
-		reset();
+		
 	}
-	
+	/*if statements to determine what will happen as player starts hitting keys*/ 
 	}
 
 
