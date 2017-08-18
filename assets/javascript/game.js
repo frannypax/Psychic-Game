d1f0770e2a64d2a651d491b3c9ba305e2b375e8e
yaw owusu <asirifiyaw@gmail.com>
2017-08-17 19:49:59 -0700

    computer-generated-letter bug fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,24 @@
 
 
 var letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var randomLetter = letter[Math.floor(Math.random() * letter.length)];
-console.log(randomLetter);
+// var randomLetter = letter[Math.floor(Math.random() * letter.length)];
+// console.log(randomLetter);
+
+var updateLetterToGuess = function() {
+  this.letterToGuess = this.letter[Math.floor(Math.random() * this.letter.length)];
+
+  console.log(letterToGuess);
+};
+
+updateLetterToGuess();
+//lines 7-13; function that makes computer randomly generate a number//
 
 var wins = 0;
 var losses = 0;
 var guessesLeft = 5;
 var guessedLetters = [];
 
-document.getElementById("wins").innerHTML = "Wins: " + wins;
-document.getElementById("losses").innerHTML = "Losses: " + losses;
-document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
-document.getElementById("guessesSoFar").innerHTML = "Guesses So Far: " + guessedLetters;
-
-var reset = function(){
+var initGame = function(){
 	guessesLeft=5;
 	guessedLetters=[];
 	document.getElementById("wins").innerHTML = "Wins: " + wins;
@@ -22,6 +26,8 @@ var reset = function(){
 	document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
 	document.getElementById("guessesSoFar").innerHTML = "Guesses So Far: " + guessedLetters;
 };
+
+initGame();
 
 	onkeyup=function(event){
 	var playerGuess = event.key.toLowerCase();
@@ -36,13 +42,15 @@ var reset = function(){
 	document.getElementById("guessesSoFar").innerHTML = "Guesses So Far: " + guessedLetters;
 	/*This will dynamically update guesses so far on the HTML page"*/
 
-	if (guessesLeft > 0 && playerGuess == randomLetter){
+	if (guessesLeft > 0 && playerGuess == letterToGuess){
 		wins++;
 		document.getElementById("wins").innerHTML = "Wins: " + wins;
-		reset();
+		initGame();
+		updateLetterToGuess();
 	}
 	if (guessesLeft == 0){
-		reset();
+		initGame();
+		updateLetterToGuess(),
 		losses++;
 		document.getElementById("losses").innerHTML = "Losses: " + losses;
 		
