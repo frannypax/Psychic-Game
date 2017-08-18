e323c81c84bdabafd61b190df51693f15589d069
yaw owusu <asirifiyaw@gmail.com>
2017-08-17 18:16:09 -0700

    game reset fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,22 +9,19 @@ var losses = 0;
 var guessesLeft = 5;
 var guessedLetters = [];
 
-var reset = function(){
-		guessesLeft=5;
-		guessedLetters=[];
-	};
-
-
-
 document.getElementById("wins").innerHTML = "Wins: " + wins;
 document.getElementById("losses").innerHTML = "Losses: " + losses;
 document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
 document.getElementById("guessesSoFar").innerHTML = "Guesses So Far: " + guessedLetters;
 
-
-
-
-
+var reset = function(){
+	guessesLeft=5;
+	guessedLetters=[];
+	document.getElementById("wins").innerHTML = "Wins: " + wins;
+	document.getElementById("losses").innerHTML = "Losses: " + losses;
+	document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+	document.getElementById("guessesSoFar").innerHTML = "Guesses So Far: " + guessedLetters;
+};
 
 	onkeyup=function(event){
 	var playerGuess = event.key.toLowerCase();
